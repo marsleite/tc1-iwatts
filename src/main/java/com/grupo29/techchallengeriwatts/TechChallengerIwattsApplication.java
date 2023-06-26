6fe1bd4bd6d2db5e07d35b568724cf0cf30657e5
Marcelo <marcelo.leite@mercadolivre.com>
2023-06-26 20:06:41 -0300

    [feat] created scaffolding iwatts

---
+++ src/main/java/com/grupo29/techchallengeriwatts/TechChallengerIwattsApplication.java
@@ -0,0 +1,13 @@
+package com.grupo29.techchallengeriwatts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengerIwattsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TechChallengerIwattsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/grupo29/techchallengeriwatts/TechChallengerIwattsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo29.techchallengeriwatts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengerIwattsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
