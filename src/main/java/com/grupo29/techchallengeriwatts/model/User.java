73b2ec90fb5d16b4e9fd617470cc9efb0f212936
Marcelo <marcelo.leite@mercadolivre.com>
2023-06-28 11:27:43 -0300

    [feat] remove @Data and include @Getter @Setter and @NoArgsConstructor

--- src/main/java/com/grupo29/techchallengeriwatts/model/User.java
+++ src/main/java/com/grupo29/techchallengeriwatts/model/User.java
@@ -3,10 +3,15 @@ package com.grupo29.techchallengeriwatts.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class User {
