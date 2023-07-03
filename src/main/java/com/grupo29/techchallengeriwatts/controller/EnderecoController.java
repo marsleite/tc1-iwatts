d6dd0d8d8353650924313e54798d34451e0b5224
Caio Lacerda <caiolacerda88@gmail.com>
2023-07-03 09:54:45 -0300

    Voltar id do endereço no corpo da resposta

--- src/main/java/com/grupo29/techchallengeriwatts/controller/EnderecoController.java
+++ src/main/java/com/grupo29/techchallengeriwatts/controller/EnderecoController.java
@@ -1,5 +1,6 @@
 package com.grupo29.techchallengeriwatts.controller;
 
+import com.grupo29.techchallengeriwatts.domain.Endereco;
 import com.grupo29.techchallengeriwatts.dto.EnderecoDTO;
 import com.grupo29.techchallengeriwatts.repository.gateway.EnderecoRepository;
 import jakarta.validation.ConstraintViolation;
@@ -9,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Set;
@@ -35,8 +33,8 @@ public class EnderecoController {
             return ResponseEntity.badRequest().body(violations);
         }
 
-        enderecoRepository.createAddress(enderecoDTO.toEndereco());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        Endereco endereco = enderecoRepository.createAddress(enderecoDTO.toEndereco());
+        return new ResponseEntity("Endereço com id %d criado.".formatted(endereco.getId()), HttpStatus.CREATED);
     }
 
     private <T> Map<Path, String> validar(T dto) {
--- src/main/java/com/grupo29/techchallengeriwatts/repository/gateway/EnderecoRepository.java
+++ src/main/java/com/grupo29/techchallengeriwatts/repository/gateway/EnderecoRepository.java
@@ -3,6 +3,5 @@ package com.grupo29.techchallengeriwatts.repository.gateway;
 import com.grupo29.techchallengeriwatts.domain.Endereco;
 
 public interface EnderecoRepository {
-
-  Endereco createAddress(Endereco endereco);
+    Endereco createAddress(Endereco endereco);
 }
